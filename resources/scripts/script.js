4897bef17e9667e65741b5a47e0c3e47a1265b4e
David-Js <davidi.ohis25@gmail.com>
2020-11-21 04:57:28 -1200

    fixing bugs in beyondztm.css and style.css removing on used id in script.js

--- resources/scripts/script.js
+++ resources/scripts/script.js
@@ -62,18 +62,15 @@ $(function () {
 
 const navbar = document.querySelector('.navbar');
 const navlink = document.querySelectorAll('.nav-link');
-const headertext = document.querySelector('#headerText');
  
 const navScroll = () => {
 
 			if (document.documentElement.scrollTop > 80) {
 				navbar.classList.add('navbar-light');
 				navbar.classList.remove('navbar-dark');
-				headertext.classList.add('header-text-light');
 			} else {
 				navbar.classList.remove('navbar-light');
 				navbar.classList.add('navbar-dark');
-				headertext.classList.remove('header-text-light');
 			}
 }
 
@@ -84,7 +81,7 @@ window.addEventListener('scroll', navScroll);
 $(window).scroll(function() {
     var height = $(window).scrollTop();
     if (height > 90) {
-        $('.gotopbtn').fadeIn();
+        $('.').fadeIn();
     } else {
         $('.gotopbtn').fadeOut();
     }
